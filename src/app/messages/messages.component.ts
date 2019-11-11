39b16d5b3ab7e7169e8cd3d421bb0f75c9a2e15e
tracomclass <classtracom@gmail.com>
2019-11-11 15:56:23 +0300

    first commit

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../hero-detail/message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+  constructor(public messageService: MessageService) { }
+
+  ngOnInit() {
+  }
+
+}
